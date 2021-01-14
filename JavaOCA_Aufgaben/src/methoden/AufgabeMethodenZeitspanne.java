7be7101bef0bd2e8b8a7116f564ea7c0c10ec472
CC-Student <CC-Student@192.168.0.185>
2021-01-14 15:14:09 +0100

    Neue Aufgabe

---
+++ JavaOCA_Aufgaben/src/methoden/AufgabeMethodenZeitspanne.java
@@ -0,0 +1,21 @@
+package methoden;
+
+/*
+ * # Aufgabe 'Methoden - Zeitspanne'
+
+	- Definieren Sie eine statische Methode "getZeitspanneInGanzenTagen", an die man als Argumente zwei Datumsangaben (zu Tag 1 und Tag 2) übergibt.
+	  Die Methode liefert dann die Anzahl der ganzen Tage zwischen den zwei Daten.
+	> Ein Datum kann man durch drei Angaben beschreiben: Tag, Monat, Jahr 
+	> Tipp: bei der Lösung ist es sinnvoll weitere Hilfsmethoden zu definieren, z.B. "istSchaltjahr"
+
+	- Optional. Lösen Sie die Aufgabe mit Hilfe der Klasse `java.util.Calendar`
+	- Optional. Lösen Sie die Aufgabe mit der java.time-API (Java SE 8)
+ */
+
+public class AufgabeMethodenZeitspanne {
+
+	public static void main(String[] args) {
+
+	}
+
+}
