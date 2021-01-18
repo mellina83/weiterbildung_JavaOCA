80f1e8d4875fa619e800bd7854f7c363eb5dbd9b
CC-Student <CC-Student@192.168.0.185>
2021-01-18 08:01:03 +0100

    Edit

--- JavaOCA_Aufgaben/src/methoden/KleineAufgabeUeberladen.java
+++ JavaOCA_Aufgaben/src/methoden/KleineAufgabeUeberladen.java
@@ -1,15 +1,20 @@
 package methoden;
 
-public class AufgabeUeberladen {
+public class KleineAufgabeUeberladen {
 
 	public static void main(String[] args) {
 		
 		/*
 		 * Kleine Aufgabe:
 		 * 
+		 * A1:
 		 * Entwickeln Sie bitte eine Methode 'getMax', die aus zwei int-Paramter
 		 * den größeren zurückliefert.
 		 * Sind die Parameter gleich, wird einer davon zurückgeliefert.
+		 * 
+		 * A2:
+		 * Wie A1, nur mit drei int-Parametern, und dabei die Methode von A1 nutzen
+		 * --> Überladung von getMax
 		 */
 		
 		int x = getMax(22, -7); // return 22
@@ -20,17 +25,19 @@ public class AufgabeUeberladen {
 		
 		x = getMax(-7, -7); //return -7
 		System.out.println(x);
-
-	}
+		
+	} // Ende Main
 	
 	static int getMax(int x1, int x2) {
-		if (x1 > x2) {
+		return x1 > x2 ? x1: x2;
+		
+		/* Meine Lösung:
+		if (x1 >= x2) {
 			return x1;
-		} else if (x1 < x2) {
-			return x2;
 		} else {
-			return x1;
+			return x2;
 		}
+		*/
 	}
 	
 
